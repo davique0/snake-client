97cd6a2972c30068bf84aebfc1fe331e5a46a275
davique0 <davique@gmail.com>
2022-11-16 16:57:03 -0500

    file created, connecting to snek server and printing /connected to server

---
+++ play.js
@@ -0,0 +1,22 @@
+const net = require("net");
+
+//establishes a connection with the game server
+const connect = function () {
+  const conn = net.createConnection({
+    host: "localhost",
+    port: 50541,
+  });
+
+  //interpret incoming data as text
+  conn.setEncoding("utf8");
+
+  //prints a message when connections has been established
+  conn.on("connect", () => {
+    console.log("Connected to server")
+  })
+
+  return conn;
+};
+
+console.log("Connecting ...");
+connect();
