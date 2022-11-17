e94b9fe4b3b7b3a953c0f1cd48dab44379268c48
davique0 <davique@gmail.com>
2022-11-17 14:49:06 -0500

    setInput function is now receiving connct return as and argument

--- play.js
+++ play.js
@@ -3,8 +3,8 @@ const {connect} = require('./client');
 const {setupInput} = require('./input');
 
 console.log("Connecting ...");
-connect();
+setupInput(connect());
 
 
 
-setupInput();
+//setupInput();
