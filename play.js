c3d69dbe33def7d12e84cfd9a13eb226a3dc53ad
davique0 <davique@gmail.com>
2022-11-17 13:34:11 -0500

    functions setupInput and handleUserInput removed and input file required

--- play.js
+++ play.js
@@ -1,34 +1,10 @@
 const { stdin } = require('process');
-const {net, connect} = require('./client');
+const {connect} = require('./client');
+const {setupInput} = require('./input');
 
 console.log("Connecting ...");
 connect();
 
-// setup interface to handle user input from stdin
-
-const setupInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-
-  stdin.on("data", handleUserInput)
-
-  return stdin;
-};
 
 
-const handleUserInput = (data) => {
-  
-  if ( data === '\u0003' ) {
-    // ctrl-c ( end of text )
-    console.log('user exits')
-    process.exit();
-  }
-  
-  if(data === 'w') {
-    console.log('Move up')
-  }
-};
-
-setupInput()
+setupInput();
