6c250a0dd59bf604b2d255786122c92cc6de0a7c
davique0 <davique@gmail.com>
2022-11-17 14:48:11 -0500

    created connection variable, and link conn object to be able to communicate with server

--- input.js
+++ input.js
@@ -1,7 +1,11 @@
-const {net, connect} = require('./client');
+const {connect} = require('./client');
 // setup interface to handle user input from stdin
 
-const setupInput = function () {
+let connection;
+
+const setupInput = (conn) => {
+  connection = conn;
+  console.log(connection);
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
@@ -14,16 +18,35 @@ const setupInput = function () {
 
 
 const handleUserInput = (data) => {
-  
-  if ( data === '\u0003' ) {
-    // ctrl-c ( end of text )
-    console.log('user exits')
-    process.exit();
+
+  switch (data) {
+    case '\u0003':
+      console.log('bye bye');
+      process.exit();
+    case 'w':
+      connection.write('Move: up');
+      break;
+    case 'a':
+      connection.write('Move: left');
+      break;
+    case 's':
+      connection.write('Move: down');
+      break;
+    case 'd':
+      connection.write('Move: right');
+      break;
   }
   
-  if(data === 'w') {
-    console.log('Move up')
-  }
+  // if ( data === '\u0003' ) {
+  //   // ctrl-c ( end of text )
+  //   console.log('user exits')
+  //   process.exit();
+  // }
+  
+  // if(data === 'w') {
+  //   console.log('Move :up')
+  // }
+
 };
 
 module.exports = {setupInput};
